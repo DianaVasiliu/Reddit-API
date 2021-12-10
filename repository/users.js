07fbc7c2f63e98226092e91462d41cce5fb41bd5
Vasiliu Diana Elena <diana.vasiliu10@gmail.com>
2021-12-11 00:25:57 +0200

    Fixed project structure & paths

--- repository/users.js
+++ repository/users.js
@@ -7,9 +7,9 @@ const getAllUsers = async () => {
         const allUsers = await db.User.findAll()
         return allUsers
     } catch (error) {
-        console.error('Something went wrong');
-        return null;
-      } 
+        console.error('Something went wrong')
+        return null
+    }
 }
 
 const getUserById = async (id) => {
@@ -17,11 +17,11 @@ const getUserById = async (id) => {
 
     try {
         const selectedUser = await db.User.findByPk(userID)
-        return selectedUser 
+        return selectedUser
     } catch (error) {
-        console.error('Something went wrong');
-        return null;
-      } 
+        console.error('Something went wrong')
+        return null
+    }
 }
 
 const createUser = async (args) => {
@@ -36,78 +36,89 @@ const createUser = async (args) => {
 
         return newUser
     } catch (error) {
-        console.error(error);
-        return null;
-      }
+        console.error(error)
+        return null
+    }
 }
 
 const updateUser = async (args, context) => {
-    const { user } = context;
-  
-  if(!user) {
-    console.log("Tried to update current user without being logged in. (without having a token in Authorization header)\n")
-    return null;
-  }
+    const { user } = context
 
-  const { id } = user;
-  
-  const { email, username, password } = args;
+    if (!user) {
+        console.log(
+            'Tried to update current user without being logged in. (without having a token in Authorization header)\n'
+        )
+        return null
+    }
 
-  try {
-    await db.User.update({
-      email,
-      username,
-      password,
-    }, { where:  id=id  });
+    const { id } = user
 
-    return await db.User.findByPk(id);
+    const { email, username, password } = args
 
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+    try {
+        const userInDB = await db.User.findByPk(id)
+
+        if (!userInDB) {
+            throw new Error('User not found')
+        }
+
+        await db.User.update(
+            {
+                email,
+                username,
+                password,
+                updatedAt: new Date(),
+            },
+            { where: (id = id) }
+        )
+
+        return await db.User.findByPk(id)
+    } catch (e) {
+        console.error(e)
+        return null
+    }
 }
 
 const deleteUser = async (id) => {
     const userID = id
 
     try {
+        const user = await db.User.findByPk(userID)
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+
         await db.User.destroy({
             where: {
                 id: userID,
             },
         })
-        return { result: "User deleted succesfully."}
+        return { result: 'User deleted succesfully.' }
     } catch (e) {
-        console.error(e);
-        return null;
-      }
+        console.error(e)
+        return null
+    }
 }
 
-//TODO: decide on parameter names for userId and communityId and update queryType 
+//TODO: decide on parameter names for userId and communityId and update queryType
 const subscribeToCommunity = async () => {
     // const userId = req.params.userId
     // const communityId = req.params.communityId
     // let error = 'Something went wrong'
-
     // try {
     //     const user = await db.User.findByPk(userId)
     //     const community = await db.Community.findByPk(communityId)
-
     //     if (!user || !community) {
     //         throw new Error('User or community not found')
     //     }
-
     //     await user.setCommunities(community)
-
     //     const updatedUser = await db.User.findByPk(userId)
     //     const tags = await updatedUser.getCommunities()
-
     //     const response = {
     //         ...updatedUser.toJSON(),
     //         tags,
     //     }
-
     //     res.status(201).send(response)
     // } catch (e) {
     //     console.error('Error:', e.message)
@@ -117,11 +128,162 @@ const subscribeToCommunity = async () => {
     // }
 }
 
+const updateSubscription = async (req, res) => {
+    const userId = parseInt(req.params.userId)
+    const communityId = parseInt(req.params.communityId)
+
+    try {
+        const user = await db.User.findByPk(userId)
+        const community = await db.Community.findByPk(communityId)
+
+        if (!user || !community) {
+            throw new Error('User or community not found')
+        }
+
+        const userIsSubscribed = await user.hasCommunity(community)
+
+        if (userIsSubscribed) {
+            await user.removeCommunity(community)
+        } else {
+            await user.setCommunities(community)
+        }
+
+        const updatedUser = await db.User.findByPk(userId)
+        const subscriptions = await updatedUser.getCommunities()
+
+        const response = {
+            ...updatedUser.toJSON(),
+            subscriptions,
+        }
+
+        res.status(201).send(response)
+    } catch (e) {
+        console.error('Error:', e.message)
+        res.send({
+            error: 'Something went wrong',
+        })
+    }
+}
+
+const toggleAdminOrModerator = async (option, req, res) => {
+    const communityId = parseInt(req.params.communityId)
+    const userId = parseInt(req.params.userId)
+    var criteria
+
+    try {
+        if (option === 'admin') {
+            criteria = {
+                isAdmin: 1,
+            }
+        } else if (option === 'moderator') {
+            criteria = {
+                isModerator: 1,
+            }
+        } else {
+            throw new Error('Invalid option')
+        }
+
+        // check if the user we want to update is a member of the community
+        const user = await db.UserCommunity.findOne({
+            where: {
+                userId,
+                communityId,
+            },
+        })
+
+        if (!user) {
+            throw new Error(
+                'There required user/community does not exist or is not a member of the community'
+            )
+        }
+
+        // check if we want to remove the only admin
+        // if the option is 'admin', then we count the admins of the community
+        if (option === 'admin') {
+            // if we want to remove the current admin
+            if (user.toJSON().isAdmin === 1) {
+                // check if this admin is the creator of the community
+                const creator = await db.Community.findOne({
+                    where: {
+                        id: communityId,
+                        userId,
+                    },
+                })
+
+                if (creator) {
+                    throw new Error('Cannot remove the creator as admin')
+                }
+
+                // check if he is the only admin of the community
+                const users = await db.UserCommunity.findAll({
+                    where: {
+                        communityId,
+                        isAdmin: 1,
+                    },
+                })
+
+                // if we only have one admin, we cannot remove it
+                if (users.length <= 1) {
+                    throw new Error('Cannot have community without admin')
+                } else {
+                    await db.UserCommunity.update(
+                        {
+                            isAdmin: 0,
+                        },
+                        {
+                            where: {
+                                userId,
+                                communityId,
+                            },
+                        }
+                    )
+                }
+            } else {
+                await db.UserCommunity.update(
+                    {
+                        isAdmin: 1,
+                        isModerator: 1,
+                    },
+                    {
+                        where: {
+                            userId,
+                            communityId,
+                        },
+                    }
+                )
+            }
+        } else {
+            if (user.toJSON().isAdmin === 1) {
+                throw new Error('Cannot update moderator role of an admin')
+            }
+            await db.UserCommunity.update(
+                {
+                    isModerator: 1 - user.toJSON().isModerator,
+                },
+                {
+                    where: {
+                        userId,
+                        communityId,
+                    },
+                }
+            )
+        }
+
+        res.status(201).send('User role updated successfully')
+    } catch (e) {
+        console.error('Error:', e.message)
+        res.send({
+            error: 'Something went wrong',
+        })
+    }
+}
+
 module.exports = {
     getAllUsers,
     getUserById,
     createUser,
     updateUser,
     deleteUser,
-    subscribeToCommunity,
+    updateSubscription,
+    toggleAdminOrModerator,
 }
